2df790ee0d27b4c22717c106fe2eb29084d5f855
vanta blanta <vantablanta@gmail.com>
2022-04-27 14:33:32 +0300

    cloned from faith

--- app/views.py
+++ app/views.py
@@ -1,7 +1,23 @@
 from flask import render_template
 from app import app
 
+# Views UI 
 
-@app.route("/")
+# @app.route("/")
+# def home():
+#     pageName = "Home"
+#     return render_template("home.html",pageName=pageName)
+
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
+
+@app.route("/about")
+def aboutUs():
+    message = "Our Vision Goes Here"
+    return render_template('about.html',message=message)
+
